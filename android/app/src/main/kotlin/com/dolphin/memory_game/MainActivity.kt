ef9469cd6efeca9e1c8c64905bd770207133024d
jake <pcx474@gmail.com>
2023-04-20 08:34:35 +0900

    개인정보처리방침 page

--- android/app/src/main/kotlin/com/dolphin/memory_game/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dolphin.memory_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/data.dart
+++
@@ -1,4 +0,0 @@
-class Data {
-  static get maxStage => 5;
-  static get maxRound => 10;
-}
